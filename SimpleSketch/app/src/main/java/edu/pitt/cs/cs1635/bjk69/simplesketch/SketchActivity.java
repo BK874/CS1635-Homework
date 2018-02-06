9e14049a12d1f93488219baafff98224f70f51c8
Brian Knotten <bjk69@pitt.edu>
2018-02-06 18:39:29 -0500

    Individual Assignment 2

---
+++ SimpleSketch/app/src/main/java/edu/pitt/cs/cs1635/bjk69/simplesketch/SketchActivity.java
@@ -0,0 +1,13 @@
+package edu.pitt.cs.cs1635.bjk69.simplesketch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SketchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sketch);
+    }
+}
